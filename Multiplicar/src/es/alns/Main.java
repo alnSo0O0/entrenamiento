d7f034a3fd1d0b179a125a6d061109f3d10221bf
Alonso Cano <firdoby@gmail.com>
2022-01-31 10:39:28 +0100

    tablica de multipla

---
+++ Multiplicar/src/es/alns/Main.java
@@ -0,0 +1,20 @@
+package es.alns;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i;
+int n;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Que tabla de multiplicar quieres saber?");
+        i = entrada.nextInt();
+
+        while( i < 10 ){
+            System.out.println( + i + " x " +  = );
+            n++;
+        }
+    }
+}
