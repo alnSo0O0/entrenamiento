2dd8417f25ffc82c1c24c45ae74f2b40aea7337c
Alonso Cano <firdoby@gmail.com>
2022-03-21 10:38:31 +0100

    cuenta caracteres en una frase

--- src/es/alns/Main.java
+++ src/es/alns/Main.java
@@ -1,16 +1,29 @@
 package es.alns;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        char letra;
+        String texto;
+        int contador = 0;
 
-	for (int i = 0; i < 6;i++){
-        for (int j = 0; j <= i; j++){
+        Scanner entrada = new Scanner(System.in);
 
-            System.out.print("0 ");
+        System.out.println("Escribe una frase (intro para terminar): ");
+        texto = entrada.nextLine();
+        System.out.println("dime una letra: ");
+        letra = entrada.nextLine().charAt(0);
 
+        for (int i = 0; i < texto.length(); i++) {
+            if (texto.toLowerCase().charAt(i) == letra) {
+                contador++;
+
+            }
         }
-        System.out.println();
-    }
+        System.out.println("Número de veces que aparece el carácter " + letra + " : " + contador );
+
     }
 }
